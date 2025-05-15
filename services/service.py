6762d1db70a284ddcc7c105585f004e12be26c51
Bhanu chander <bhanujirra@gmail.com>
2025-05-15 08:59:46 +0530

    handled network error

--- services/service.py
+++ services/service.py
@@ -1,12 +1,13 @@
+import time
 from playwright.sync_api import sync_playwright
 import asyncio
 import sys
 from fastapi import HTTPException
 
-if sys.platform.startswith('win'):
-    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+# if sys.platform.startswith('win'):
+#     asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
-def is_table_empty(page, table_selector) -> bool:
+def is_table_empty(table_selector) -> bool:
    
     if not table_selector.count():
         return True
@@ -25,78 +26,83 @@ def login_to_mri(page, username, password):
     pass_word.press_sequentially(password) #Enter the password
     button = page.get_by_role("button", name="Login")
     button.click()
-    error_message = page.locator("#msgError")
-    print("Error message: ", error_message.inner_text())
-    if error_message.is_visible(timeout = 200):
+    
+    
+    if page.locator("#msgError").is_visible():
         print("Invalid Credentials")
-        raise HTTPException(status_code=401, detail=error_message.inner_text())
+        raise HTTPException(status_code=401, detail=page.locator("#msgError").inner_text())
     else:
         print("Login successful!")
     return page
+
 def auto_mri(usernam_e, passwor_d):
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False, slow_mo = 100)
+        browser = playwright.chromium.launch(headless=False, slow_mo = 300)
         context = browser.new_context(
             permissions=["geolocation"]  # Denying geolocation permissions
         )
+        page = context.new_page() #create new page
+        try:
+            page = login_to_mri(page, usernam_e, passwor_d) #login to mri
 
-        page = context.new_page()
-        page = login_to_mri(page, usernam_e, passwor_d)
-        page.wait_for_load_state("load")
-        page.locator("#repaeaterdata_A1_0").click() #application processing
-        page.wait_for_load_state("load")
-        page.locator("#Repeater1_a1_1").click() #community
-        page.wait_for_load_state("load")
-        page.locator("#cmbProblemStatus").select_option(value="14") #selecting the authority
-        print("Authority selected!")
-        page.locator("#btnSearch").click() #search
-        print("Search button clicked!")
+            page.wait_for_load_state("load", timeout=5000)
+        
+            page.locator("#repaeaterdata_A1_0").click() #application processing
+            page.wait_for_load_state("load", timeout=5000)
+            page.locator("#Repeater1_a1_1").click() #community
+            page.wait_for_load_state("load", timeout=5000)
+            page.locator("#cmbProblemStatus").select_option(value="14") #selecting the authority
+            print("Authority selected!")
+            page.locator("#btnSearch").click() #search
+            print("Search button clicked!")
 
-        page.wait_for_load_state("load")
-        table = page.locator("#gvCustomers")
+            page.wait_for_load_state("load", timeout=5000)
+            table = page.locator("#gvCustomers")
 
-        rows = table.locator("tbody tr").count()
-        print("Number of rows in the table: " + str(rows))
-        i = 1
-        while not is_table_empty(table):
+            rows = table.locator("tbody tr").count()
+            print("Number of rows in the table: " + str(rows))
             
-            link_locator = table.locator("tbody tr:nth-child(1) td:nth-child(2) a")
-            link_locator.click()
-            page.wait_for_load_state("load")
-            
-            page.wait_for_selector("#txtDateOfBirth")
+            while not is_table_empty(table) :
+                
+                link_locator = table.locator("tbody tr:nth-child(1) td:nth-child(2) a")
+                link_locator.click()
 
-            # Remove readonly attribute and clear the field
-            page.evaluate("document.getElementById('txtDateOfBirth').removeAttribute('readonly')")
-            page.evaluate("document.querySelector('#txtDateOfBirth').setAttribute('value', '')")
-            # page.fill("#txtDateOfBirth", "")
-            page.wait_for_load_state("load")
-            print("Date of Birth field cleared!" + str(i))
-            i += 1
-            with page.expect_popup() as new_page_info:
-                page.locator("#lnlCheckList").click() #checklist 
+                page.wait_for_load_state("load", timeout=5000)
+                page.wait_for_selector("#txtDateOfBirth")
 
-            new_page = new_page_info.value  # Get the new page reference
-            new_page.wait_for_load_state("load")  # Wait for the new page to fully load
+                # Remove readonly attribute and clear the field
+                page.evaluate("document.getElementById('txtDateOfBirth').removeAttribute('readonly')")
+                page.evaluate("document.querySelector('#txtDateOfBirth').setAttribute('value', '')")
+                # page.fill("#txtDateOfBirth", "")
+                page.wait_for_load_state("load", timeout=5000)
 
-            
-            new_page.locator("#ddlconduct").select_option(value = "Yes") 
-            new_page.locator("#txtSanction").fill("Yes") 
-            new_page.locator("#txtRecordSt").fill("Yes")
-            new_page.locator("#txtMediator").fill("Yes") 
-            new_page.locator("#ddlRecommVRO").select_option(value = "Recommonded") 
-            new_page.locator("#txtVerificationVRO").fill("Yes") 
-            new_page.locator("#txtVeriRPA").fill("Yes") 
-            new_page.locator("#ddlRecommRI").select_option(value = "Recommonded")
+                with page.expect_popup() as new_page_info:
+                    page.locator("#lnlCheckList").click() #checklist 
 
-            new_page.click("#btnSubmit")  # Submit the form
-            new_page.wait_for_load_state("load")  # Wait for the response
-            new_page.close()  # Close the current tab
-            
-            page.locator("#txtMRIRemarks").fill("recommended                                                              ")
-            page.click("#btnFwdDYMRO")  # Click the button using its ID
-            page.wait_for_load_state("load")  # Wait for submission to complete
-            page.locator("#btnBack").click()  # Go back to the previous page
-            page.wait_for_load_state("load")  # Wait for the page to load
-        page.wait_for_timeout(50)
-        browser.close()
+                new_page = new_page_info.value  # Get the new page reference
+                new_page.wait_for_load_state("load", timeout=5000)  # Wait for the new page to fully load
+
+                
+                new_page.locator("#ddlconduct").select_option(value = "Yes") 
+                new_page.locator("#txtSanction").fill("Yes") 
+                new_page.locator("#txtRecordSt").fill("Yes")
+                new_page.locator("#txtMediator").fill("Yes") 
+                new_page.locator("#ddlRecommVRO").select_option(value = "Recommonded") 
+                new_page.locator("#txtVerificationVRO").fill("Yes") 
+                new_page.locator("#txtVeriRPA").fill("Yes") 
+                new_page.locator("#ddlRecommRI").select_option(value = "Recommonded")
+
+                new_page.click("#btnSubmit")  # Submit the form
+                new_page.wait_for_load_state("load", timeout=5000)  # Wait for the response
+                new_page.close()  # Close the current tab
+                
+                page.locator("#txtMRIRemarks").fill("recommended                                                              ")
+                page.click("#btnFwdDYMRO")  # Click the button using its ID
+                page.wait_for_load_state("load", timeout=5000)  # Wait for submission to complete
+                page.locator("#btnBack").click()  # Go back to the previous page
+                page.wait_for_load_state("load", timeout= 5000)  # Wait for the page to load
+            page.wait_for_timeout(50)
+        except TimeoutError:
+            raise HTTPException(status_code=408, detail="Request timed out")
+        finally:
+           browser.close()
